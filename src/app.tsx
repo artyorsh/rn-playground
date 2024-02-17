629ff1734eab5fc3609a409823af9222f399c433
Artur <artyorsh.me@gmail.com>
2024-02-17 22:41:31 +0100

    move app.tsx to src

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { ProductDetailsContainer } from './src/screens/product-details/product-details.container';
-import { AppModule, lazyInject } from './src/di/container';
-import { INavigationService } from './src/service/navigation/model';
+
+import { AppModule, lazyInject } from './di/container';
+import { INavigationService } from './service/navigation/model';
 
 export class App extends React.Component {
 

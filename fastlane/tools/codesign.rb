770afa848aa552587fbeffb31fcf836b45746f4e
Artur Y <artur@artyorsh.me>
2025-04-23 12:25:56 +0200

    Revert "Revert "use import_certificate""
    This reverts commit 544ceb1b294fa0bc6cb72c8ffba637cc17d352c0.

--- fastlane/tools/codesign.rb
+++ fastlane/tools/codesign.rb
@@ -16,6 +16,11 @@ def ios_hack_development_codesigning(options)
   entitlements_path = "#{IOS_DIR}/#{PROJECT_NAME}/#{PROJECT_NAME}.entitlements"
   File.write(entitlements_path, {}.to_plist)
 
+  import_certificate(
+    certificate_path: ENV['FASTLANE_IOS_CERT_PATH'],
+    certificate_password: ENV["FASTLANE_IOS_CERT_PASSWORD"],
+  )
+
   install_provisioning_profile(
     path: ENV['FASTLANE_IOS_PROVISIONING_PROFILE_PATH'],
   )

476ccaa9badaddbe1335f6b8b1177ab93c3d59e3
Artur Y <artur@artyorsh.me>
2025-07-10 10:05:46 +0200

    react-native-mmkv --> expo-secure-store
    https://github.com/mrousavy/react-native-mmkv/issues/840

--- src/auth/session/index.ts
+++ src/auth/session/index.ts
@@ -7,7 +7,7 @@ import { ILogService } from '@/log';
 import { ISessionModule } from './initialzier';
 import { ParallelModuleInitializer } from './initialzier/parallel-module-initializer';
 import { LocalAuthenticationProvider } from './local-auth-provider';
-import { MMKVAuthenticationStorage } from './mmkv-auth-storage';
+import { SecureAuthStorage } from './secure-auth-storage';
 import { ISessionInitializer, SessionService } from './session.service';
 
 export interface ISession {
@@ -39,9 +39,7 @@ export const SessionServiceFactory = (context: interfaces.Context): ISessionServ
   return new SessionService({
     tokenRefreshThresholdMinutes: Number(process.env.EXPO_PUBLIC_RNAPP_AUTH_TOKEN_REFRESH_THRESHOLD_MINUTES) || 0,
     authenticationProvider: new LocalAuthenticationProvider(),
-    authenticationStorage: new MMKVAuthenticationStorage({
-      encryptionKey: process.env.EXPO_PUBLIC_RNAPP_STORAGE_ENCRYPTION_KEY,
-    }),
+    authenticationStorage: new SecureAuthStorage(),
     initializer: sessionInitializer,
     logger: logService,
   });

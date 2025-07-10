259befecd1289b11fd1cbdae49c2b71ce81170ca
Artur Y <artur@artyorsh.me>
2025-07-10 18:10:52 +0200

    enable prebuild and cgn, reconfigure react-native-firebase
    https://docs.expo.dev/workflow/continuous-native-generation/
    
    https://rnfirebase.io/\#configure-react-native-firebase-modules

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import registerRootComponent from 'expo/src/launch/registerRootComponent';
 
-import {AppRegistry} from 'react-native';
 import 'core-js/proposals/reflect-metadata';
 import { App } from './src/app';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
-// registerRootComponent(App);
+registerRootComponent(App);
